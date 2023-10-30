336c6bcc3bd62b70c3ba3f6b3a4fef8e1dc06a90
MiltonC3 <miltoncoria03@gmail.com>
2023-10-30 13:01:56 -0300

    terminada la primera actividad, html basico

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.resolve("public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("views", "home.html"));
+});
+
+app.listen(8080, (req, res) => {
+    console.log("listening on: http://localhost:8080");
+});
