56581d8a8cb4859d986cdf9dad3029a488ed55c6
MiltonC3 <miltoncoria03@gmail.com>
2023-11-06 11:01:11 -0300

    corregi la cuarta actividad

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const port = 3000
 
 app.use(express.static(path.resolve("public")));
-
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("views", "home.html"));
 });
 
-app.listen(8080, (req, res) => {
-    console.log("listening on: http://localhost:8080");
+app.listen(port, (req, res) => {
+    console.log(`listening on: http://localhost:${port}`);
 });
