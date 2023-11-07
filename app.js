af92fbf3fccaf969e9b6c4b720f070e71ade6e01
MiltonC3 <miltoncoria03@gmail.com>
2023-11-06 23:42:09 -0300

    terminada la quinta actividad, form

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.use(express.static(path.resolve("public")));
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("views", "home.html"));
 });
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve("views", "login.html"));
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve("views", "register.html"));
+});
 
 app.listen(port, (req, res) => {
     console.log(`listening on: http://localhost:${port}`);
